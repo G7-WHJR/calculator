1d635c30afa6126578f64163b6238c58064f95a4
unknown <raveen.anand@whitehatjr.com>
2021-10-22 12:21:53 +0530

    v2

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add() {
+
+    var input1=Number(document.getElementById("n1").value);
+    var input2=Number(document.getElementById("n2").value)
+    var result = input1 + input2;
+    document.getElementById("answer").innerHTML=result;
+}
+function subtract() {
+
+    var input1=Number(document.getElementById("n1").value);
+    var input2=Number(document.getElementById("n2").value)
+    var result = input1 - input2;
+    document.getElementById("answer").innerHTML=result;
+}
+
+function multiply() {
+
+    var input1=Number(document.getElementById("n1").value);
+    var input2=Number(document.getElementById("n2").value)
+    var result = input1 * input2;
+    document.getElementById("answer").innerHTML=result;
+}
+
+function divide() {
+
+    var input1=Number(document.getElementById("n1").value);
+    var input2=Number(document.getElementById("n2").value)
+    var result = input1 / input2;
+    document.getElementById("answer").innerHTML=result;
+}
+
+
+
+    
+    
